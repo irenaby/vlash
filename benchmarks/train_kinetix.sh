0dff9f91aa83d3797ef08a68ec3a23e1afb4fdac
irenaby <irena.byzalov@arm.com>
2026-04-30 12:06:54 +0300

    update benchmark/kinetix/scripts/train.sh and save it as benchmark/train_kinetix.sh (in the main repo)

---
+++ benchmarks/train_kinetix.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# modified benchmark/kinetix/scripts/train.sh (benchmark/kinetix is a submodule)
+
+# Train flow policy with async delay augmentation
+
+: "${N_GPUS:=4}"
+: "${EXPERT_DATA:=/app/data}"
+: "${ASYNC_INTERVAL:=5}"
+
+default_outdir=/outputs/train_vlash_kinetix_async${ASYNC_INTERVAL}_gpus${N_GPUS}
+: "${OUTDIR:=$default_outdir}"
+
+export NCCL_NET_MERGE_LEVEL=LOC
+export NCCL_P2P_DISABLE=1
+export NCCL_IB_DISABLE=1
+export NCCL_DEBUG=WARN
+
+export CUDA_VISIBLE_DEVICES=$(seq -s, 0 $((NUM_GPUS-1)))
+
+python src/train_flow.py \
+    --config.run-path $EXPERT_DATA \
+    --config.async-interval $ASYNC_INTERVAL \
+    --config.output-dir $OUTDIR
--- setup.sh
+++ setup.sh
@@ -12,7 +12,8 @@ source "$HOME/miniforge3/etc/profile.d/conda.sh"
 conda create -n kinetix python==3.11
 conda activate kinetix
 
-cd /dev/shm
+REPO_ROOT=/dev/shm
+cd $REPO_ROOT
 git clone https://github.com/irenaby/vlash.git vlash_fork && cd vlash_fork && git checkout mykinetix
 git submodule update --init --recursive
 cd benchmarks/kinetix/third_party/kinetix
@@ -21,11 +22,20 @@ cd ../..
 pip uninstall -y jax jax-cuda12-pjrt jax-cuda12-plugin jaxlib
 pip install jax==0.4.34 jax-cuda12-pjrt==0.4.34 jax-cuda12-plugin==0.4.34 jaxlib==0.4.34 flax
 
-hf download mit-han-lab/vlash-kinetix-policy-async5
-snapshots=/root/.cache/huggingface/hub/models--mit-han-lab--vlash-kinetix-policy-async5/snapshots
-ckpt_path=$snapshots/$(ls $snapshots)
-cd benchmarks/kinetix
-python src/eval_flow.py --run-path $ckpt_path --output-dir /outputs/eval_outputs
+# there is a mismatch between the ckpt and the code
+#hf download mit-han-lab/vlash-kinetix-policy-async5
+#snapshots=/root/.cache/huggingface/hub/models--mit-han-lab--vlash-kinetix-policy-async5/snapshots
+#ckpt_path=$snapshots/$(ls $snapshots)
+#cd benchmarks/kinetix
+#python src/eval_flow.py --run-path $ckpt_path --output-dir /outputs/eval_outputs
+
+pip install gsutil
+mkdir /app/data
+gsutil -m rsync -r gs://rtc-assets/expert/ /app/data/
+
+export WANDB_MODE=disabled
+cd $REPO_ROOT/benchmark/kinetix
+EXPERT_DATA=/app/data OUTDIR=/outputs N_GPUS=4 bash ../train_kinetix.sh    # this is a modified copy of scripts/train.sh from benchmark/kinetix git submodule
 
 #pip install mujoco==3.3.7 numpy==1.24.4 || echo foo
 sleep 5d
