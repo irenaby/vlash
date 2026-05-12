45e24addf90401aea5ab92119475653f8de2c34a
irenaby <irena.byzalov@arm.com>
2026-05-12 09:04:51 +0300

    update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -ex
 
-apt-get update && apt-get install -y wget vim
+apt-get update && apt-get install -y wget vim less
 
 wget -O /tmp/Miniforge3-Linux-x86_64.sh \
   https://github.com/conda-forge/miniforge/releases/latest/download/Miniforge3-Linux-x86_64.sh
@@ -15,4 +15,21 @@ conda env create -f ./conda_env.yaml
 conda activate vlash
 pip install -e .
 pip install mujoco==3.3.7 numpy==1.24.4 || echo foo
+
+
+mkdir -p /root/.libero
+cat > /root/.libero/config.yaml <<'EOF'
+assets: /root/miniforge3/envs/vlash/lib/python3.10/site-packages/libero/libero/./assets
+bddl_files: /root/miniforge3/envs/vlash/lib/python3.10/site-packages/libero/libero/./bddl_files
+benchmark_root: /root/miniforge3/envs/vlash/lib/python3.10/site-packages/libero/libero
+datasets: /root/miniforge3/envs/vlash/lib/python3.10/site-packages/libero/libero/../datasets
+init_states: /root/miniforge3/envs/vlash/lib/python3.10/site-packages/libero/libero/./init_files
+EOF
+
+cd /dev/shm/vlash_fork
+#export HF_TOKEN=<TOKEN>
+#DETERMINISTIC_ENABLED=true N_ACTION_STEPS=5 DELAYS="0 1 2 3 4" NUM_GPUS=8 COMPILE=true bash libero-eval-scripts/run.sh > /outputs/vlash_libero_determ_actionsteps5_delay0-4.t 2>&1 &
+#pid=$!
+#wait $pid
+
 sleep 5d
